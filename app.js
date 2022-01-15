2aa96d026831f103b0e159b7a54c613b4bd3b107
keithlimmontajes19 <keith@noblemen.io>
2022-01-16 01:02:50 +0800

    'PORT'

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 /* COMPONENTS */
-// import userRoute from './routes/userRoute';
-// import authRoute from './routes/authRoute';
-// import connection from './config/connection';
-// import {authorize} from './helpers/authorizeHelper';
-
-/* CONNECTION */
-// connection(app);
-const PORT = process.env.PORT || 8080;
-app.listen(PORT);
+import userRoute from './routes/userRoute';
+import authRoute from './routes/authRoute';
+import connection from './config/connection';
+import {authorize} from './helpers/authorizeHelper';
 
 /* MIDDLEWARE */
 app.use(cors());
 app.use(bodyParser.json());
+app.listen(PORT);
 
-app.get('/api/test', (req, res) => {
-    res.send('hi');
-});
+/* CONNECTION */
+connection(app);
+
 /* ROUTES */
-// app.use('/api/auth', authRoute);
-// app.use('/api/user', authorize, userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/user', authorize, userRoute);
