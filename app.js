a4c4e1c8c3263c63b6e8916877e1d14eac8e7bb0
keithlimmontajes19 <keith@noblemen.io>
2022-01-16 00:41:04 +0800

    'sample'

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ import bodyParser from 'body-parser';
 const app = express();
 
 /* COMPONENTS */
-import userRoute from './routes/userRoute';
-import authRoute from './routes/authRoute';
-import connection from './config/connection';
-import {authorize} from './helpers/authorizeHelper';
+// import userRoute from './routes/userRoute';
+// import authRoute from './routes/authRoute';
+// import connection from './config/connection';
+// import {authorize} from './helpers/authorizeHelper';
 
 /* CONNECTION */
-connection(app);
+// connection(app);
 
 /* MIDDLEWARE */
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(8000);
 
+app.get('/api/test', (req, res) => {
+    res.send('hi');
+});
 /* ROUTES */
-app.use('/api/auth', authRoute);
-app.use('/api/user', authorize, userRoute);
+// app.use('/api/auth', authRoute);
+// app.use('/api/user', authorize, userRoute);
+app.listen(8000);
