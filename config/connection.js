6082122b047c4ab7beacccbde8fc57e0173e5ef4
keithlimmontajes19 <keith@noblemen.io>
2022-01-16 01:09:53 +0800

    'env'

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const mongoConnect = async (app) => {
+const mongoConnect = async () => {
     try {
         await mongoose.connect(
             process.env.DB_CONNECTION,
